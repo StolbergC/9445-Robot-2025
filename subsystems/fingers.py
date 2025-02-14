d5caabc52685c9a769f60703dc6f10e9f5df72ac
Bryson Smith <15smith.bryson@gmail.com>
2025-02-14 11:47:16 -0600

    Modify fingers--follow may not work

--- subsystems/fingers.py
+++ subsystems/fingers.py
@@ -3,27 +3,34 @@ from commands2 import RunCommand, Subsystem
 
 
 class Fingers(Subsystem):
-    def __init__(self):
+    def __init__(self, amp_limit: int = 60):
         self.left_motor = SparkMax(30, SparkMax.MotorType.kBrushless)
-        # self.right_motor = SparkMax(31, SparkMax.MotorType.kBrushless)
-        motor_config = SparkMaxConfig().smartCurrentLimit(60)
+        self.right_motor = SparkMax(31, SparkMax.MotorType.kBrushless)
+        motor_config = SparkMaxConfig().smartCurrentLimit(amp_limit)
         self.left_motor.configure(
             motor_config,
             SparkBase.ResetMode.kResetSafeParameters,
             SparkBase.PersistMode.kNoPersistParameters,
         )
-        # motor_config = motor_config.follow(30, True)
-        # self.right_motor.configure(
-        #     motor_config,
-        #     SparkBase.ResetMode.kResetSafeParameters,
-        #     SparkBase.PersistMode.kNoPersistParameters,
+        # motor_config_right = (
+        #     SparkMaxConfig().smartCurrentLimit(amp_limit).follow(self.left_motor, True)
         # )
+        self.right_motor.configure(
+            motor_config,
+            SparkBase.ResetMode.kNoResetSafeParameters,
+            SparkBase.PersistMode.kNoPersistParameters,
+        )
+
+    def set_motors(self, power: float) -> None:
+        power = 1 if power > 1 else -1 if power < -1 else power
+        self.left_motor.set(power)
+        self.right_motor.set(-power)
 
     def stop(self) -> RunCommand:
-        return RunCommand(lambda: self.left_motor.set(0), self)
+        return RunCommand(lambda: self.set_motors(0), self)
 
     def intake(self) -> RunCommand:
-        return RunCommand(lambda: self.left_motor.set(0.25), self)
+        return RunCommand(lambda: self.set_motors(0.25), self)
 
     def score(self) -> RunCommand:
-        return RunCommand(lambda: self.left_motor.set(-0.25), self)
+        return RunCommand(lambda: self.set_motors(-0.25), self)
