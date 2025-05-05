b49dab79170d564237ac5b65bacf0a896f2520a4
9445 <stolbergc@usd204.net>
2025-05-05 15:50:32 -0500

    iowa, move to offseason

---
+++ auto/red_1l1.py
@@ -0,0 +1,38 @@
+from auto import positions
+from subsystems.drivetrain import Drivetrain
+
+from wpimath.geometry import Pose2d, Rotation2d
+from commands2 import Command, DeferredCommand, RepeatCommand
+
+from subsystems.elevator import Elevator
+from subsystems.wrist import Wrist
+from subsystems.claw import Claw
+from subsystems.fingers import Fingers
+
+from commands import score_l1
+
+
+def get_auto(
+    drivetrain: Drivetrain,
+    elevator: Elevator,
+    wrist: Wrist,
+    claw: Claw,
+    fingers: Fingers,
+) -> Command:
+    return (
+        drivetrain.reset_pose(positions.blue_start_line_center)
+        .andThen(
+            RepeatCommand(
+                DeferredCommand(
+                    lambda: drivetrain.drive_joystick(
+                        lambda: -0.3, lambda: 0, lambda: 0, lambda: False  # True
+                    )
+                )
+            )
+            .withTimeout(3.75)
+            .alongWith(score_l1.score_l1_on_true(elevator, wrist).andThen(claw.coral()))
+        )
+        .andThen(drivetrain.stop())
+        .andThen(fingers.score().withTimeout(5))
+        .withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming)
+    )
